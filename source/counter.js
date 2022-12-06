9e9b46fe3e6b038f5013e489cf55037b864b1307
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-12-06 13:47:01 -0800

    testing smile

--- source/counter.js
+++ source/counter.js
@@ -29,4 +29,3 @@ function init(){
         count.innerHTML = clicks.total;
     });
 }
-module.exports = {click, resetCounter, clicks};
--- source/counter.test.js
+++ source/counter.test.js
@@ -1,17 +1,14 @@
-const fn = require("./counter.js");
+describe('e2e tests', () => {
+    beforeAll(async () => {
+        await page.goto('http://127.0.0.1:5500/source/');
+    })
 
-test('Increment Test', () => {
-    fn.click();
-    expect(fn.clicks.total).toBe(1);
-});
-
-test('2 Increment Test', () => {
-    fn.click();
-    fn.click();
-    expect(fn.clicks.total).toBe(3);
-});
-
-test('Reset Test', () => {
-    fn.resetCounter();
-    expect(fn.clicks.total).toBe(0);
+    test('simple button test', async () => {
+        let button = await page.$('#increment-button');
+        await button.click();
+        let clickCount = await page.$('#click-count');
+        let clickCountText = await clickCount.getProperty('innerText');
+        console.log('here');
+        expect(await clickCountText.jsonValue()).toBe("1");
+    });
 });
